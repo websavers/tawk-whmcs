8e4dbda06418cfeada549408ff7c9270b3c1b5ce
Jordan Schelew <jordan@websavers.ca>
2025-01-15 14:54:48 -0400

    Open a ticket in WHMCS when creating one in Tawk
    A few notes on this:
    1. There is code here to assoc the ticket with the client, but we should never use it because we have no way of knowing the client is authenticated (they're likely not).
    2. This will leave tickets open in Tawk as there's no way to close them after the webhook runs

---
+++ modules/addons/tawkto/webhooks.php
@@ -0,0 +1,91 @@
+<?php
+
+/**
+ * This file is designed to be accessed directly via Tawk.to webhooks
+ * Docs: https://developer.tawk.to/webhooks/
+ * To use this file, upload it to your WHMCS tawk folder under modules/addons/tawkto
+ * 1. Go to dashboard.tawk.to > Administration > Settings > Webhooks
+ * 2. Create Webhook and specify New Ticket event
+ * 3. Enter Request Endpoint URL as: URL_TO_WHMCS/modules/addons/tawkto/webhooks.php
+ * 4. Update the variable below with the Secret Key that Tawk provides (be sure you do this on the live copy of this file)
+**/
+
+const WEBHOOK_SECRET = 'webhook secret key';
+
+$body = file_get_contents('php://input');
+
+function verifySignature ($body, $signature) {
+    $digest = hash_hmac('sha1', $body, WEBHOOK_SECRET);
+    return $signature === $digest ;
+}
+if (!verifySignature($body, $_SERVER['HTTP_X_TAWK_SIGNATURE'])) {
+    // verification failed
+    die('Verification Failed');
+}
+// verification success
+
+// Import WHMCS
+require( "../../../init.php" );
+
+// Database access
+//use Illuminate\Database\Capsule\Manager as Capsule;
+//use WHMCS\Database\Capsule;
+
+$tawk_data = json_decode($body);
+
+switch($tawk_data->event){ //4 possible events as follows
+
+    case 'chat:start':
+    case 'chat:end':
+    case 'chat:transcript_created':
+        //Do nothing
+        break;
+
+    case 'ticket:create':
+
+        if (!filter_var($tawk_data->requester->email, FILTER_VALIDATE_EMAIL)) {
+            die('Invalid email address provided');
+        }
+
+        $user_id = false;
+        $client_id = false;
+/*
+        $w_u_results = localAPI('GetUsers', array('search' => $tawk_data->requester->email));
+        if ($w_u_results['result'] === 'success'){
+            $user = $w_u_results['users'][0];
+            $user_id = $user['id'];
+            foreach($user['clients'] as $client){
+                if ($client['isOwner']){
+                    $client_id = $client['id'];
+                }
+            }
+        }
+*/
+        $open_ticket_data = array(
+            'deptid' => '1',
+            'subject' => $tawk_data->ticket->subject,
+            'message' => $tawk_data->ticket->message,
+            'priority' => 'Medium',
+            'markdown' => false,
+        );
+
+        if ( $client_id ) $open_ticket_data['clientid'] = $client_id;
+        if ( $client_id && $user_id ) $open_ticket_data['userid'] = $user_id;
+        if ( !$client_id ){
+            $open_ticket_data['email'] = $tawk_data->requester->email;
+            $open_ticket_data['name'] = $tawk_data->requester->name;
+        }
+
+        $whmcs_results = localAPI('OpenTicket', $open_ticket_data);
+        if ( $whmcs_results['result'] == 'success' ){
+            logActivity("[Tawk Webhook] Created ticket ID " . $whmcs_results['id']);
+        }
+        else{
+            logActivity("[Tawk Webhook] Failed to create ticket from Tawk ticket ID " . $tawk_data->ticket->id);
+        }
+
+        break;
+
+    default: 
+}
+//http_response_code(200);
